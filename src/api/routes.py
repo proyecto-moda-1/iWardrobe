f54b5866b1c37d5a8b1b3447a1c37b0a6d629654
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-31 19:52:15 +0000

    primeras dos tablas hechas

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,31 +2,33 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Clothing
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user', methods=['POST', 'GET'])
+def get_all_users():
+    all_users = User.query.all()
+
+    serialized_users = []
+    for user in all_users:
+        serialized_users.append(user.serialize())
+    print(all_users)
+
+    return jsonify(serialized_users), 200
+    
+@api.route('/clothing', methods=['GET', 'POST'])
+def get_all_clothing():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend"
+        "msg": "Hello, this is your GET /user response "
     }
 
     return jsonify(response_body), 200
-    
-# @app.route('/clothing', methods=['GET', 'POST'])
-# def clothing_hello():
 
-#     response_body = {
-#         "msg": "Hello, this is your GET /user response "
-#     }
-
-#     return jsonify(response_body), 200
-
-# @app.route('/outfits', methods=['GET', 'POST'])
+# @api.route('/outfits', methods=['GET', 'POST'])
 # def outfits_hello():
 
 #     response_body = {
