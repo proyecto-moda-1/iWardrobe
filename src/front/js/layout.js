57058533f29bf83609d1aae7cb412860c73830d3
mgtablante <mgtablantel@gmail.com>
2021-06-02 15:28:16 +0000

    1er cambio login-singup

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,7 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-
+import { Login } from "./pages/login";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -29,8 +29,9 @@ const Layout = () => {
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
-						{/* <Route component={Login} path="/login" /> */}
-						<Route exact path="/single/:theid">
+						<Route exact path="/login" />
+						<Login />
+						<Route exact path="/single/:login">
 							<Single />
 						</Route>
 						<Route>
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,18 +1,159 @@
-// import React, { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import PropTypes from "prop-types";
 
-// export default function Login() {
-//     const [username, setUsername] = useState("")
-//     const [password, setPassword] = useState("")
+import "../../styles/login.scss";
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//     }
-// }
+export const Login = function(props) {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-// return (
-//     <form onSubmit={handleSubmit}>
-//         <input placeholder="username" onChange={e => setUsername(e.target.value)} value={username} />
-//         <input type="password" placenholder="password" onChange={e => setPassword(e.target.value)} value={password} />
-//         <button>Login</button>
-//     </form>
-// )
+	const history = useHistory();
+
+	const handleSubmit = () => {
+		const data = {
+			email: email,
+			password: password
+		};
+	};
+
+	return (
+		<div className="row">
+			<div className="col-md-6 mx-auto p-0">
+				<div className="card">
+					<div className="login-box">
+						<div className="login-snip">
+							{" "}
+							<input id="tab-1" type="radio" name="tab" className="sign-in" checked />
+							<label className="tab-1 tab">Login</label>{" "}
+							<div className="login-space">
+								<div className="login">
+									<div className="group">
+										{" "}
+										<label className="user label">NickName</label>{" "}
+										<input
+											type="email"
+											className="input"
+											id="exampleFormControlInput1"
+											placeholder="tucorreo@ejemplo.com"
+											value={email}
+											onChange={event => setEmail(event.target.value)}
+										/>{" "}
+									</div>
+									<div className="group">
+										{" "}
+										<label className="pass label">Password</label>{" "}
+										<input
+											type="password"
+											className="input"
+											id="exampleFormControlInput1"
+											placeholder="introduce tu contraseña"
+											value={password}
+											onChange={event => setPassword(event.target.value)}
+										/>{" "}
+									</div>
+									<div className="group">
+										{" "}
+										<input id="check" type="checkbox" className="check" checked />{" "}
+										<label className="check">
+											<span className="icon" /> Keep me Signed in
+										</label>{" "}
+									</div>
+									<div className="group">
+										{" "}
+										<input type="submit" className="button" value="Sign In" />{" "}
+									</div>
+									<div className="hr" />
+									<div className="foot">
+										{" "}
+										<a href="#">Forgot Password?</a>{" "}
+										<div className="sign-up-form">
+											<div className="group">
+												{" "}
+												<input id="tab-2" type="radio" name="tab" className="sign-up" />
+												<div className="singup">
+													<label className="tab-2tab">Sign Up</label>
+													<label className="user label">NickName</label>{" "}
+													<input
+														type="email"
+														className="input"
+														id="exampleFormControlInput1"
+														placeholder="NickName"
+														value={email}
+														onChange={event => setEmail(event.target.value)}
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<label className="pass label">Password</label>{" "}
+													<input
+														type="password"
+														className="input"
+														id="exampleFormControlInput1"
+														placeholder="introduce tu contraseña"
+														value={password}
+														onChange={event => setPassword(event.target.value)}
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<label className="pass label">Repeat Password</label>{" "}
+													<input
+														id="pass"
+														type="password"
+														className="input"
+														data-type="password"
+														placeholder="Repeat your password"
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<label className="pass label">Email Address</label>{" "}
+													<input
+														id="pass"
+														type="text"
+														className="input"
+														placeholder="Enter your email address"
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<label className="pass label">Gender</label>{" "}
+													<input
+														id="pass"
+														type="text"
+														className="input"
+														placeholder="Gender"
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<label className="pass label">Imagen</label>{" "}
+													<input
+														id="pass"
+														type="text"
+														className="input"
+														placeholder="Imagen"
+													/>{" "}
+												</div>
+												<div className="group">
+													{" "}
+													<input type="submit" className="button" value="Sign Up" />{" "}
+												</div>
+												<div className="hr" />
+												<div className="foot">
+													{" "}
+													<label className="tab-1">Already Member?</label>{" "}
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
