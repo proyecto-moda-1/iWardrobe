55389d7f8c4e5bedb0b525128f472476637395f8
yolandagb <yolandagarciabarranco@hotmail.com>
2021-06-16 09:43:50 +0000

    modal working just a bit of styles to be done

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,41 @@
-const getState = ({ getStore, getActions, setStore }) => {
+
+const getState = ({ getStore, setState, getActions }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			clothing: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
+			createClothing: data => {
 				const store = getStore();
+				const endpoint = process.env.BACKEND_URL + "/api/clothing";
+				const config = {
+					method: "POST",
+					body: JSON.stringify({
+						user_id: 1,
+						name: data.name,
+						category: data.category,
+						image:
+							"https://th.bing.com/th/id/R9e6e1694bdbb9d0148c1d5d451b7169b?rik=A70S6pmmXpjrGA&pid=ImgRaw"
+					}),
+					headers: {
+						"Content-Type": "application/json",
+						"Access-Control-Allow-Origin": "*"
+					}
+				};
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				fetch(endpoint, config)
+					.then(response => {
+						setName("");
+						setImage("");
+						setCategory("");
+						return response.json();
+					})
+					.then(json => console.log(json))
+					.catch(err => console.error(err));
 			}
 		}
 	};
 };
-
 export default getState;
+
