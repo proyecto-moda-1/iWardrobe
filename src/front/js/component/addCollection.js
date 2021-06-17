f8ed301d462d1706bf57bdc43190e54f46fd4f33
yolandagb <yolandagarciabarranco@hotmail.com>
2021-06-17 16:05:03 +0000

    some errors to fix

--- src/front/js/component/addCollection.js
+++ src/front/js/component/addCollection.js
@@ -1,19 +1,17 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import "../../styles/LoadClothing.scss";
+import "../../styles/addCollection.scss";
 import { Context } from "../store/appContext";
 import { getActions } from "../store/flux.js";
 
 const AddCollection = props => {
 	const { show, handleClose } = props;
-	// const [name, setName] = useState("");
+	const [name, setName] = useState("");
 	const [image, setImage] = useState("");
-	const [category, setCategory] = useState("");
+	const [collection_user_id, setUser] = useState("");
 	const { store, actions } = useContext(Context);
 
 	const handleSubmit = () => {
@@ -28,8 +26,20 @@ const AddCollection = props => {
 	return (
 		<Modal show={show} onHide={handleClose}>
 			<Modal.Header closeButton>
-				<Modal.Title>Add Clothing</Modal.Title>
+				<Modal.Title>New Collection</Modal.Title>
 			</Modal.Header>
+            <div className="group-user-id">
+				{" "}
+				<label className="pass label" />{" "}
+				<input
+					type="text"
+					className="input-image"
+					id="clothing-image-url"
+					placeholder="User Id"
+					value={collection_user_id}
+					onChange={event => setUser(event.target.value)}
+				/>{" "}
+			</div>
 			<div className="group-clothing">
 				{" "}
 				<label className="pass label" />{" "}
@@ -37,22 +47,11 @@ const AddCollection = props => {
 					type="text"
 					className="input-clothing"
 					id="clothing-name"
-					placeholder="Clothing's Name"
+					placeholder="Collections Name"
 					value={name}
 					onChange={event => setName(event.target.value)}
 				/>{" "}
 			</div>
-			<Form.Control
-				size="sm"
-				as="select"
-				className="select-clothing"
-				value={category}
-				onChange={event => setCategory(event.target.value)}>
-				<option value="0">Category</option>
-				<option value="top">Top</option>
-				<option value="bottom">Bottom</option>
-				<option value="footwear"> Footwear</option>
-			</Form.Control>
 			<div className="group-image">
 				{" "}
 				<label className="pass label" />{" "}
