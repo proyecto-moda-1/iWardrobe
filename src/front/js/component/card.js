a45f86231b0d41cacc8b0f2ac3c17441be3d2e5a
mgtablante <mgtablantel@gmail.com>
2021-06-19 01:37:19 +0000

    No tengo respuesta de la base de datos cara generar lo disponible

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -5,28 +5,26 @@ import "../../styles/card.scss";
 //create your first component
 
 export function Card(props) {
-   const params = useParams();
+	const params = useParams();
 
 	const { store, actions } = useContext(Context);
 
-    const history = useHistory();
-    
-    useEffect(() => {
-		actions.getAllOutfit(); 
-	}, []);
+	const history = useHistory();
 
+	useEffect(() => {
+		actions.getAllOutfit();
+	}, []);
 
 	return (
 		<div className="card">
-			<img
-				className="card-img"
-				src={props.imagen}
-			/>
+			<Link to={`/outfit/1${props.imagen}`} className="text-dark text-decoration-none" />
 			<div className="card-body">
 				<h5 className="card-title text-center">{props.collections}</h5>
 				{props.clothingItems}
 				<div className="btn-wrapper text-center d-flex justify-content-between">
-					<a className="btn btn-outline-primary  btn-sm text-primary d-flex align-items-center">{props.name}</a>
+					<a className="btn btn-outline-primary  btn-sm text-primary d-flex align-items-center">
+						{props.name}
+					</a>
 					<a className="btn btn-outline-danger text-danger"> ♡</a>
 				</div>
 			</div>
@@ -37,5 +35,6 @@ export function Card(props) {
 Card.propTypes = {
 	collections: PropTypes.string,
 	name: PropTypes.string,
-	clothingItems : PropTypes.string
+	clothingItems: PropTypes.string,
+	imagen: PropTypes.string
 };
--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { Card } from "react-bootstrap";
+import { Card } from "../component/card.js";
 import { getActions } from "../store/flux.js";
 
 import PropTypes from "prop-types";
@@ -8,37 +8,24 @@ import PropTypes from "prop-types";
 import "../../styles/profile.scss";
 
 export const Profile = () => {
-	// const params = useParams();
+	const { store, actions } = useContext(Context);
 
-	// const { store, actions } = useContext(Context);
+	useEffect(() => {
+		actions.getAllOutfit();
+	}, []);
 
-	// useEffect(() => {
-	//     actions.getAllOutfit();
-	// }, []);
-	// let
-
+	let outfitCards = [];
+	if (store.profile.length == 0) {
+		outfitCards = store.allOutfit.map((allOutfits, index) => {
+			return (
+				<Card name={post.name} collections={post.collections} clothingItems={post.clothingItems} key={index} />
+			);
+		});
+	}
 	return (
-		// <Card
-		//     name={name}
-		//     collections={collections}
-		//     clothingItems={collectionsItems}
-		// />
-
-		<div className="container-fluid">
-			<div className="row justify-content-md-center">
-				<div className="col rounded float-star">
-					<img
-						src="https://image.freepik.com/vector-gratis/mujer-joven-ordenador-portatil-escritorio-trabajando-linea-ilustracion_24877-69119.jpg"
-						className="rounded float-start"
-						alt=""
-					/>
-				</div>
-				<div className="col">
-					<p className="col-md-auto">
-						MODA Es lo que compras. ESTILO es lo que haces con eso. No lo dejes para ultima hora.
-					</p>
-				</div>
-			</div>
+		<div className="home">
+			<h1 className="text-warning"> Characters</h1>
+			<div className="planet d-flex flex-row">{outfitCards}</div>
 		</div>
 	);
 };
