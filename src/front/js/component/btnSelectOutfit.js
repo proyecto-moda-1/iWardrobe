c1f90cf20677a97c801925b7210500b5c8b78182
yolandagb <yolandagarciabarranco@hotmail.com>
2021-07-02 14:20:13 +0000

    hay qye meterle la lgica un pco y el formato ceckbox

---
+++ src/front/js/component/btnSelectOutfit.js
@@ -0,0 +1,34 @@
+import React, { Component, useState, useEffect, useContext } from "react";
+import { getActions } from "../store/flux.js";
+import { Context } from "../store/appContext";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Form } from "react-bootstrap";
+// queremos hacer un cehckbox asi que debemos primero buscar como hacer el checkbox y desoues coger y meterle la lógoca, sera un ochnage con un usestate y un useeffect
+
+export const CreateOutfitBtn = props => {
+	const { store, actions } = useContext(Context);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Container>
+				<Row>
+					<Col sm={true}>
+						<Button variant="outline-light" className="save-button" onClick={handleShow}>
+							Save changes
+						</Button>
+						{/* <CreateOutfit show={show} handleClose={handleClose} /> */}
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
+};
+
+export default CreateOutfitBtn;
