65726a0a305ada91bb16935ec33165e0e5376b14
mgtablante <mgtablantel@gmail.com>
2021-08-25 15:55:39 +0000

    css fix in closet and profile

--- migrations/versions/4456c864ab72_.py
+++ migrations/versions/4456c864ab72_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 114502c41d72
+Revision ID: 4456c864ab72
 Revises: 
-Create Date: 2021-08-24 10:19:02.025122
+Create Date: 2021-08-25 15:08:34.724823
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '114502c41d72'
+revision = '4456c864ab72'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/closet.js
+++ src/front/js/pages/closet.js
@@ -34,21 +34,23 @@ export const Closet = () => {
 	}
 	return (
 		<div className="container">
-			<h1 className="display-4">Your closet</h1>
+			<h1 className="tittleCloset">Your Closet</h1>
 			<p className="lead">Dress for every ocassion</p>
 			<div className="my-4">
 				<div className="row">
+					<h1 className="favouritesText">- FAVOURITES</h1>
+					<div className="planet d-flex flex-row">{favoriteCards}</div>
+				</div>
+				<div className="row">
 					<div className="col-xs-3">
-						<h1 className="text"> COLLECTIONS</h1>
-						<CollectionDropdown />
+						<h1 className="collectionsText">- COLLECTIONS</h1>
+						<div className="collectionDropdown">
+							<CollectionDropdown />
+						</div>
 					</div>
 					{/* <div className="col-xs-9">
 						<div className="collectionOutfit d-flex flex-row">{outfitsByCategory}</div>
 					</div> */}
-				</div>
-				<div className="row">
-					<h1 className="text">FAVOURITES</h1>
-					<div className="planet d-flex flex-row">{favoriteCards}</div>
 				</div>
 				<div className="row">
 					<h1 className="text">{store.selectedCollection.name}</h1>
